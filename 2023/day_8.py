7821c42dc06c30dbe1c7f3b20ef8680447ce11ef
Jodi Diffey <17diffeyj@lytchett.org.uk>
2023-12-08 17:16:45 +0000

    day 8 part 1

--- 2023/day_8.py
+++ 2023/day_8.py
@@ -1,3 +1,24 @@
 import aoc
+import itertools
 
 data = aoc.import_input(8)
+# data  = """LLR
+
+# AAA = (BBB, BBB)
+# BBB = (AAA, ZZZ)
+# ZZZ = (ZZZ, ZZZ)"""
+
+path, nodes = data.split("\n\n")
+nodes = nodes.split("\n")
+nodes = [node.split(" = ") for node in nodes]
+graph = {}
+cur = "AAA"
+for node in nodes:
+    graph[node[0]] = node[1][1:-1].split(", ")
+
+for i, d in enumerate(itertools.cycle(path)):
+    dir_idx = 0 if d == "L" else 1
+    cur = graph[cur][dir_idx]
+    if cur == "ZZZ":
+        break
+print(i+1)
