ffc2dcdf6fd9939570162f6d1e777a520612b9fe
Jodi Diffey <119941206+purpleysound@users.noreply.github.com>
2024-11-29 21:35:49 +0000

    more utils maybe theyll actually be useful and save me time copium

--- 2024/python/aoc.py
+++ 2024/python/aoc.py
@@ -22,6 +22,28 @@ def get_nums_negative(string:str):
     """Returns a list of all numbers in a string, including but not exclusive to negative numbers"""
     return list(map(int, re.findall(r"-?\d+", string)))
 
+def pairs_adjacent(iterable):
+    iterable = iter(iterable)
+    prev = next(iterable)
+    for item in iterable:
+        yield prev, item
+        prev = item
+
+def pairs_unique(iterable):
+    iterable = list(iterable)
+    for idx, item in enumerate(iterable):
+        for jtem in iterable[idx+1:]:
+            yield item, jtem
+
+def pairs_all(iterable, stop_i_equals_i=True):
+    iterable = list(iterable)
+    for idx, item in enumerate(iterable):
+        for jdx, jtem in enumerate(iterable):
+            if stop_i_equals_i and idx==jdx:
+                continue
+            yield item, jtem
+
+
 if __name__ == "__main__":
     import os
     os.mkdir("../inputs")
@@ -31,6 +53,4 @@ if __name__ == "__main__":
             f.write(f"""import aoc\n\ndata = aoc.import_input({str(i)})\n""")
         with open(f"../inputs/day_{str(i)}.txt", "w") as f:
             f.write("")
-        
-
-        
+    
