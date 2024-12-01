068fe9227901764dbb49c1e8b67b928f3a4b00a6
Jodi Diffey <119941206+purpleysound@users.noreply.github.com>
2024-12-01 05:08:09 +0000

    69th... nice :3

--- 2024/python/day_1.py
+++ 2024/python/day_1.py
@@ -1,3 +1,21 @@
 import aoc
 
 data = aoc.import_input(1)
+
+total = 0
+total2 = 0
+lefts = []
+rights = []
+for line in data.split("\n"):
+    a, b = aoc.get_nums_negative(line)
+    lefts.append(a)
+    rights.append(b)
+
+lefts.sort()
+rights.sort()
+for a, b in zip(lefts, rights):
+    total += abs(a-b)
+    total2 += a * rights.count(a)
+
+print(total)
+print(total2)
