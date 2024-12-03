7fea0bec3c80fb30ce251bce2f986d85dd1a9aef
Jodi Diffey <119941206+purpleysound@users.noreply.github.com>
2024-12-03 05:29:17 +0000

    since when does re.findall return a list of tuples

--- 2024/python/day_3.py
+++ 2024/python/day_3.py
@@ -1,3 +1,27 @@
 import aoc
+import re
 
 data = aoc.import_input(3)
+
+total = 0
+total2 = 0
+
+
+muls = re.findall(r"(mul\(\d+,\d+\))|(don\'t\(\))|(do\(\))", data)
+# print(muls)
+on = True
+for mul in muls:
+    nums, dont, do = mul
+    if dont:
+        on=False
+    if do:
+        on=True
+    if nums:
+        a, b = aoc.get_nums(nums)
+        total += a*b
+        if on:
+            total2 += a*b
+
+
+print(total)
+print(total2)
