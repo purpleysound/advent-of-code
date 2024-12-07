ba13d0a12f311b2ea6c4d0a5c574332cc3fe1159
Jodi Diffey <119941206+purpleysound@users.noreply.github.com>
2024-12-07 05:34:27 +0000

    gave up on itertools.permutations

--- 2024/python/day_7.py
+++ 2024/python/day_7.py
@@ -1,3 +1,47 @@
 import aoc
 
 data = aoc.import_input(7)
+
+total = 0
+total2 = 0
+
+
+for line in data.split("\n"):
+    nums = aoc.get_nums(line)
+    target, *operands = nums
+    for i in range(2**len(operands)):
+        cur = operands[0]
+        for j in range(1, len(operands)):
+            if i & (1 << j):
+                cur += operands[j]
+            else:
+                cur *= operands[j]
+        
+        if cur > target:
+            continue
+
+        if cur == target:
+            total += target
+            break
+    
+    for i in range(3**len(operands)):
+        cur = operands[0]
+        for j in range(1, len(operands)):
+            if i % 3 == 0:
+                cur += operands[j]
+            elif i % 3 == 1:
+                cur *= operands[j]
+            else:
+                cur = int(str(cur) + str(operands[j]))
+            i //= 3
+
+        if cur > target:
+            continue
+            
+        if cur == target:
+            total2 += target
+            break
+
+
+print(total)
+print(total2)
