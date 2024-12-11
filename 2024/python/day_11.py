ef1448894c435e847a254071941e22398fb39391
Jodi Diffey <119941206+purpleysound@users.noreply.github.com>
2024-12-11 05:21:02 +0000

    took way too long to think of an optimisation for part 2 i didnt even think this would work lol i tried a cache in my brute force function but doing it recursively can save a lot of steps at once which is nice

--- 2024/python/day_11.py
+++ 2024/python/day_11.py
@@ -2,4 +2,30 @@ from aoc import *
 
 data = import_input(11)
 
+data = nums(data)
 
+total = 0
+total2 = 0
+
+
+@functools.lru_cache(maxsize=None)
+def blink_recursive(num, depth):
+    if depth == 0:
+        return 1
+    digits = len(str(num))
+    if num == 0:
+        return blink_recursive(1, depth-1)
+    elif digits % 2 == 0:
+        return blink_recursive(int(str(num)[:digits//2]), depth-1) + blink_recursive(int(str(num)[digits//2:]), depth-1)
+    else:
+        return blink_recursive(num*2024, depth-1)
+    
+
+data.sort()
+for num in data:
+    total += blink_recursive(num, 25)
+    total2 += blink_recursive(num, 75)
+
+
+print(total)
+print(total2)
