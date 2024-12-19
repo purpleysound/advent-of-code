46d312b3fc9c10c46d3985d3fca7ca2b033f217c
Jodi Diffey <119941206+purpleysound@users.noreply.github.com>
2024-12-19 05:37:48 +0000

    oddly easy for day 19

--- 2024/python/day_19.py
+++ 2024/python/day_19.py
@@ -1,5 +1,29 @@
 from aoc import *
 
 data = import_input(19)
+patterns, wanted = data.split("\n\n")
+wanted = wanted.split("\n")
+patterns = patterns.split(", ")
+
+total = 0
+total2 = 0
+
+@functools.cache
+def possible(wanted:str):
+    if wanted == "":
+        return 1
+    total = 0
+    for pattern in patterns:
+        if wanted.startswith(pattern):
+            total += possible(wanted[len(pattern):])
+    return total
 
 
+for w in wanted:
+    p = possible(w)
+    if p:
+        total += 1
+    total2 += p
+
+print(total)
+print(total2)
