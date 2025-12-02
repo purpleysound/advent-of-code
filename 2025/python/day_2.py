c9388c3afbcb631d9ebc1780a0c8bb24cf4cad3d
Jodi Diffey <jodidiffey@gmail.com>
2025-12-02 10:32:45 +0000

    day 2 part 1 has some funny overoptimised definitions i think

--- 2025/python/day_2.py
+++ 2025/python/day_2.py
@@ -1,5 +1,47 @@
 from aoc import *
 
 data = import_input(2)
+# data = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+# 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+# 824824821-824824827,2121212118-2121212124"""
+
+range_texts = data.split(",")
+ranges = map(nums, range_texts)
+
+invalid_sum = 0
+
+for a, b in ranges:
+    if len(str(a)) == 1:
+        test = 0
+    else:
+        test = int(str(a)[:len(str(a)) // 2])
+    upper = int(str(b)[:len(str(b)) // 2 ])
+
+    while True:
+        consider = int(str(test)*2)
+        if consider > b:
+            break
+        if consider >= a:
+            invalid_sum += consider
+        test += 1
+
+print(invalid_sum)
 
 
+
+ranges = map(nums, range_texts)
+invalid_sum = 0
+for a, b in ranges:
+    seen = set()
+    for i in range(2, len(str(b)) + 1):
+        test = 1
+        while True:
+            consider = int(str(test)*i)
+            if consider > b:
+                break
+            if consider >= a and consider not in seen:
+                invalid_sum += consider
+                seen.add(consider)
+            test += 1
+
+print(invalid_sum)   
